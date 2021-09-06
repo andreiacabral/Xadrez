c06e8c0e7d73aa91d77c716d86c0527541a95c0a
Andreia Cabral <andreiacabraldacosta@outlook.com>
2021-09-06 09:50:30 -0300

    Melhorando a implimentação do metado imprimirTabuleiro

--- Xadrez/Program.cs
+++ Xadrez/Program.cs
@@ -10,21 +10,24 @@ namespace Xadrez
     {
         static void Main(string[] args)
         {
-            PosicaoXadrez pos = new PosicaoXadrez('a', 1);
-            Console.WriteLine(pos);
+            try
+            {
 
-            Console.WriteLine(pos.ToPosicao());
-            
-            ////
-            //Tabuleiro tab = new Tabuleiro(8, 8);
+                Tabuleiro tab = new Tabuleiro(8, 8);
 
-            //tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-            //tab.colocarPeca(new Torre(tab, Cor.Preta),new Posicao(1, 3));
-            //tab.colocarPeca(new Rei(tab, Cor.Preta),new Posicao(2, 4));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
 
+                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(3,5));
+                Tela.ImprimirTabuleiro(tab);
+            }
+            catch (TabuleiroException e)
+            {
+                Console.WriteLine(e.Message);
+                throw;
+            }
 
-
-            //Tela.ImprimirTabuleiro(tab);
 
 
             Console.ReadLine();
